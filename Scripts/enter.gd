faaaba3fb4ee1d7b3286734b8098382fec28c26b
konsvasi <konsvass@outlook.com>
2017-11-05 15:58:46 +0100

    added function to move between scenes, entering house in this case

---
+++ Scripts/enter.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+export var moveToScene = ""
+
+
+
+
+func _on_enter_home_body_enter( body ):
+	print("Enter the house", moveToScene)
+	
+	get_tree().get_root().get_node("testWorld").queue_free()
+	get_tree().get_root().add_child(load(moveToScene).instance())
+	
+	
